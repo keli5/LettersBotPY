feccbada83b2769747eecd3c310106e069a9c7aa
keli5 <kel5isgod@gmail.com>
2020-07-24 23:28:42 -0500

    yeah

--- cogs/voice.py
+++ cogs/voice.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+voicestates = {}
 
 
 class Voice(commands.Cog):
@@ -9,8 +10,22 @@ class Voice(commands.Cog):
     async def joinvc(self, ctx):
         if ctx.author.voice:
             if ctx.author.voice.channel:
-                await ctx.author.voice.channel.connect()
+                voicestate = await ctx.author.voice.channel.connect()
                 await ctx.send(f"Connected to {ctx.author.voice.channel.name}")
+                voicestates[ctx.guild.id] = voicestate
+        else:
+            await ctx.send("You are not in a voice channel.")
+
+    @commands.command(aliases=["disconnect", "dc"])
+    async def leavevc(self, ctx):
+        botmember = ctx.guild.get_member(ctx.bot.user.id)
+        if ctx.author.voice and botmember.voice:
+            if ctx.author.voice.channel is botmember.voice.channel:
+                await voicestates[ctx.guild.id].disconnect()
+                await ctx.send(f"Disconnected from {ctx.author.voice.channel.name}")
+                voicestates[ctx.guild.id] = None
+            else:
+                await ctx.send("You are not in the same voice channel as the bot.")
         else:
             await ctx.send("You are not in a voice channel.")
 
