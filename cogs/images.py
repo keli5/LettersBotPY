6e0912855269622d9a888e8b6ca80fbf208f9e20
keli5 <46039037+keli5@users.noreply.github.com>
2021-07-31 12:14:09 -0500

    "Bruh Moment"

--- cogs/images.py
+++ cogs/images.py
@@ -32,22 +32,6 @@ class Images(commands.Cog):
         out.seek(0)
         await ctx.send(file=discord.File(out, filename="grayscale.png"))
 
-    @commands.command(aliases=["image2audio", "imagetowav"])
-    async def image2wav(self, ctx, attachment=None):
-        """ Pipe the raw bytes from an image into a .wav file. """
-        source = attachment or ctx.message.attachments[0].url or None
-        image = await image_from_url(source)
-        imgbytes = image_to_byte_array(image)
-        samplingrate = 7000
-        pcsmsg = await ctx.send('Processing... this may take a few minutes')
-        async with ctx.channel.typing():
-            buffer = io.BytesIO()
-            bytes = numpy.array(imgbytes, dtype=numpy.int8)  # we'll feed this to our wav file
-            wavfile.write(buffer, samplingrate, bytes)  # this is a bad idea
-            filename = f"img2audio-{secrets.token_hex(nbytes=15)}.wav"  # generate a unique filename
-            await ctx.send(file=discord.File(fp=buffer, filename=filename))
-            await pcsmsg.delete()
-
     @commands.command(aliases=["colors"])
     async def resample(self, ctx, colors: int = 32, attachment=None):
         """ Sample an image down to <colors> colors. """
@@ -58,11 +42,10 @@ class Images(commands.Cog):
         image = await image_from_url(source)
         processing = await ctx.send('Processing image... (this could take a bit)')
         async with ctx.channel.typing():
-            width, height = image.size
             image = image.quantize(colors=colors, kmeans=colors)
             image = image.convert("RGB")
             hexcodes = b""
-            for o, (r, g, b) in image.getcolors():
+            for _, (r, g, b) in image.getcolors():
                 hexcodes += b'#%02x%02x%02x\n' % (r, g, b)
             hexes = io.BytesIO(hexcodes)
             image.save(out, "png")
