26ea6f1501552ac0e4e2efc909c4d6c0300e9356
keli5 <46039037+keli5@users.noreply.github.com>
2021-10-30 17:26:08 -0500

    clean up, add inventory, finish buy

--- cogs/shop.py
+++ cogs/shop.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-from classes.dbmodels import GuildShop
-from utility.funcs import paginate_list
+from classes.dbmodels import GuildShop, LBUser
+from utility.funcs import paginate_list, db_for_user
 
 
 async def get_shop(id):
@@ -39,24 +39,69 @@ class Shop(commands.Cog):
 
         prices = paginate_list(prices, 10, page)
         items = paginate_list(items, 10, page)  # uh, this is a bit of a hack.
-
+        if len(items) == 0:
+            shopembed.description = "No items to display."
         for item, price in zip(items, prices):
-            shopembed.add_field(name=item, value=price + self.cur)
+            shopembed.add_field(name=item, value=self.cur + str(price))
+
+        await ctx.send(embed=shopembed)
+
+    @commands.command(aliases=["inv"])
+    async def inventory(self, ctx, page: int = 1, user: discord.User = None):
+        """ Show your or another player's inventory. """
+        items = []
+        amounts = []
+        user = user or ctx.author
+        userdb = await db_for_user(user.id, True)
+        shopembed = discord.Embed(
+            title=f"Inventory of {ctx.author.name}",
+            color=discord.Color.greyple()
+        )
+        for item, amount in userdb.inventory.items():
+            items.append(item)
+            amounts.append(amount)
+
+        amounts = paginate_list(amounts, 10, page)
+        items = paginate_list(items, 10, page)
+        if len(items) == 0:
+            shopembed.description = "No items to display."
+        for item, amount in zip(items, amounts):
+            shopembed.add_field(name=item, value=amount)
 
         await ctx.send(embed=shopembed)
 
     @shop.command()
-    async def buy(self, ctx, item: str):
+    async def buy(self, ctx, item: str, amount: int = 1):
         """ Buy an item from the shop. """
+        if amount < 1:
+            return await ctx.send("You can't buy less than 1 item.")
         shop = await get_shop(ctx.guild.id)
+        user = await db_for_user(ctx.author.id, True)
+        user_inv = user.inventory
+        await ctx.send(user_inv)
+
         items = shop.items
         try:
             price = items[item]
         except KeyError:
-            await ctx.send("That item is not in the shop.")
-            return
-        
-        
+            return await ctx.send(f"{item if len(item) <= 55 else 'That item'} is not in the shop.")
+        if price * amount > user.balance:
+            return await ctx.send(f"You do not have enough money to buy {amount} {item}.")
+
+        try:
+            user_inv[item] += amount
+        except KeyError:
+            user_inv[item] = amount
+
+        await LBUser.filter(id=ctx.author.id).update(balance=user.balance - (price * amount),
+                                                     inventory=user_inv)
+        buyembed = discord.Embed(
+            title="Transaction successful!",
+            description=f"You have bought {amount} {item} for {self.cur}{price * amount}.",
+            color=discord.Color.green()
+        )
+        buyembed.add_field(name="Balance remaining", value=self.cur + str(user.balance - (price * amount)))
+        await ctx.send(embed=buyembed)
 
 
 def setup(bot):
